bc4bcb91ae5b1f9a4667a88ecc70af7e99ef7d8b
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2022-10-25 20:58:21 +0530

    Added solution - LeetHub

--- Bit Difference - GFG/bit-difference.cpp
+++ Bit Difference - GFG/bit-difference.cpp
@@ -14,16 +14,16 @@ class Solution{
     int countBitsFlip(int a, int b){
         
         int cnt = 0;
-        while(a != 0 || b!=0)
+       
+        int xori = a^b;
+        while(xori)
         {
-            int t1 = a&1;
-            int t2 = b&1;
-            if(t1 != t2)
-             cnt++;
+            if((1&xori) == 1)
+            cnt++;
             
-            a>>=1;
-            b>>=1;
+            xori>>=1;
         }
+       
         return cnt;
         
     }
