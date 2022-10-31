e4c8e424132f12c32712dd50c6e4b48732eeddef
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2022-10-31 14:20:56 +0530

    Time: 381 ms (21.55%), Space: 20.6 MB (48.92%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        
+        int n = nums.size();
+        vector<vector<int>> ans;
+        
+        unordered_map<int,int> m;   //sum, index
+        
+        sort(nums.begin(), nums.end());
+        
+        if(nums[0] > 0) return {};
+        
+        for(int i = 0; i < n; i++)
+            m[nums[i]] = i;
+        
+        
+        for(int i = 0; i < n; i++)
+        {
+            if(i > 0 && nums[i] == nums[i-1]) continue;  //for duplicates
+            
+            // if(nums[i] > 0) return ans;
+            
+            for(int j = i+1; j < n; j++)
+            {
+                if(j != i+1 && nums[j] == nums[j-1]) continue;
+                
+                int sum = -(nums[i] + nums[j]);
+                
+                if(m.count(sum) && m[sum] > j)
+                    ans.push_back({nums[i], nums[j], sum});
+                
+            }
+        }
+        
+        return ans;
+    }
+};
