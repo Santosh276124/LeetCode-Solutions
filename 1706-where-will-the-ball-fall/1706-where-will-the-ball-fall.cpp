5fb1734bb9b55133656bf333e42df9d765be5126
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2022-11-01 17:39:02 +0530

    Time: 64 ms (26.26%), Space: 13.2 MB (52.18%) - LeetHub

---
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -0,0 +1,40 @@
+class Solution {
+    int dfs(int i , int j, vector<vector<int>>& grid)
+    {
+        //base case
+        if(i >= grid.size())
+            return j;
+        
+        //left to right
+        else if(grid[i][j] == 1 && j + 1 < grid[0].size() && grid[i][j+1] == 1)
+            return dfs(i+1, j+1, grid);
+        
+        //right to left
+        else if(grid[i][j] == -1 && j-1 >= 0 && grid[i][j-1] == -1)
+            return dfs(i+1, j-1, grid);
+        
+        else if(grid[i][j] == 1 && j+1 >= grid[0].size())
+            return -1;
+        
+        else
+            return -1;
+    }
+public:
+    vector<int> findBall(vector<vector<int>>& grid) {
+        
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        vector<int> ans(m);
+        
+        for(int j = 0; j < m; j++)
+        {
+            
+           
+                ans[j] = dfs(0, j, grid);
+            
+            
+        }
+        return ans;
+    }
+};
