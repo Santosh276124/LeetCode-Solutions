32c4386fd8ba927917094e184fceff84272e689a
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2022-11-02 17:44:04 +0530

    Time: 4 ms (49.68%), Space: 6.7 MB (28.81%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -29,13 +29,32 @@ class Solution {
         dp[tar] = ans;
         return dp[tar];
     }
+    
+   int solveTab(vector<int> &nums, int tar)
+    {
+        vector<unsigned int > dp(tar+1, 0);
+         
+         dp[0] = 1;
+         
+         for(int i = 1; i <= tar; i++)
+         {
+             for(int j = 0; j < nums.size(); j++)
+             {
+                 if(i-nums[j] >= 0)
+                   dp[i] += dp[i-nums[j]];
+             }
+         }
+         
+         return dp[tar];
+    }
 public:
     int combinationSum4(vector<int>& nums, int target) {
          
         // return solve(nums, target);
         
-        vector<int> dp(target+1, -1);
+        // vector<int> dp(target+1, -1);
+        // return solveMem(nums, target, dp);
         
-        return solveMem(nums, target, dp);
+        return solveTab(nums, target);
     }
 };
