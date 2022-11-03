bd7d92551b407e0a7b766c089c1d6fd0a5fc7eae
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2022-11-03 13:54:48 +0530

    Time: 7 ms (62.64%), Space: 9.7 MB (57.37%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -45,13 +45,45 @@ class Solution {
         return dp[index];
         
     }
+    
+     int solveTab(int n , vector<int> &days, vector<int> &costs)
+    {
+      
+         vector<int> dp(n+1, INT_MAX);
+         
+         dp[n] = 0;
+         
+         for(int k = n-1; k >= 0; k--)
+         {
+            //1 day pass
+            int option1 = costs[0] + dp[k+1];
+
+            //7 day pass
+            int i;
+            for(i = k; i < n && days[i] < days[k]+7; i++);
+
+            int option2 = costs[1] + dp[i];
+
+            //30 day pass
+            for(i = k; i < n && days[i] < days[k]+30; i++);
+
+            int option3 = costs[2] + dp[i];
+
+            dp[k] = min(option1, min(option2, option3));
+         }
+         
+         return dp[0];
+        
+    }
 public:
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         
         int n = days.size();
 //         return solve(n, days, costs, 0);
         
-        vector<int> dp(n+1, -1);
-        return solveMem(n, days, costs, 0, dp);
+        // vector<int> dp(n+1, -1);
+        // return solveMem(n, days, costs, 0, dp);
+        
+        return solveTab(n, days, costs);
     }
 };
