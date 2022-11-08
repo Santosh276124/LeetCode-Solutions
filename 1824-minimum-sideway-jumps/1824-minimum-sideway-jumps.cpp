0c44af00392b8f462dcf19b57be5b84af80561af
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2022-11-08 12:16:46 +0530

    Time: 800 ms (38.19%), Space: 230.1 MB (59.70%) - LeetHub

--- 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
+++ 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
@@ -44,12 +44,48 @@ class Solution {
             return  dp[currLane][currPos];
         }
     }
+    
+     int solveTab(vector<int>& obs){
+        int n = obs.size()-1;
+       
+         vector<vector<int>> dp(4, vector<int>(obs.size(), 1e9));
+         
+         dp[0][n] = 0;
+         dp[1][n] = 0;
+         dp[2][n] = 0;
+         dp[3][n] = 0;
+         
+         for(int currPos = n-1; currPos >= 0; currPos--)
+         {
+             for(int currLane = 1; currLane <= 3; currLane++)
+             {
+                 if(obs[currPos+1] != currLane)  //seedha jayenge bc no obs present at next pos
+                    dp[currLane][currPos] = dp[currLane][currPos+1];
+                else
+                {
+                    //sidewqys jump karenge
+                    int ans = 1e9;
+                    for(int i = 1; i <= 3; i++)
+                    {
+                        if(currLane != i && obs[currPos] != i)
+                            ans = min(ans, 1 + dp[i][currPos+1]);
+                    }
+                    dp[currLane][currPos] = ans;
+                    
+                }
+             }
+         }
+       return min({dp[2][0], 1+dp[1][0], 1+dp[3][0]});
+    }
+    
 public:
     int minSideJumps(vector<int>& obstacles) {
         
         // return solve(obstacles, 2, 0);
         
-        vector<vector<int>> dp(4, vector<int>(obstacles.size(), -1));
-        return solveMem(obstacles, 2, 0, dp);
+        // vector<vector<int>> dp(4, vector<int>(obstacles.size(), -1));
+        // return solveMem(obstacles, 2, 0, dp);
+        
+        return solveTab(obstacles);
     }
 };
