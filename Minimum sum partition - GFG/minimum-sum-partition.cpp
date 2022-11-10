6175524219e0203c2393e0e3a1e8508306ab2f51
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2022-11-10 10:54:36 +0530

    Added solution - LeetHub

--- Minimum sum partition - GFG/minimum-sum-partition.cpp
+++ Minimum sum partition - GFG/minimum-sum-partition.cpp
@@ -5,48 +5,29 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
   public:
-	int minDifference(int nums[], int n)  { 
-	 
-	         int totalSum = 0;
-        for(int i=0;i<n;i++)totalSum+=nums[i];
-        
-        vector<vector<bool>> dp(n+1,vector<bool>(totalSum+1,false));
-        
-        for(int i=0;i<n;i++) dp[i][0] = true;
-        dp[0][nums[0]] = true;
-        
-        // 0 is already covered in the base case
-        for(int ind=1;ind<n;ind++) //in the recursion call we went from n-1 to 0
-        {
-            // 0 is already covered in the base case
-            for(int target = 1 ; target<=totalSum; target++)  //in the recur call we went from sum to 0
-            {
-                // copy the recurrance
-                bool take = false;
-                if(target>=nums[ind])
-                {
-                    take = dp[ind-1][target-nums[ind]];
-                }
-                
-                bool notTake = dp[ind-1][target];
-                
-                dp[ind][target] = take || notTake;
-            }
-        }
-        
-        // int the last row of dp , we have the info whether that sum is possible or not
-        int ans = 1e9;
-        for(int j=0;j<=(totalSum)/2;j++)
-        {
-            if(dp[n-1][j] == true)  //that sum is possible
-            {
-                int sum1 = j;
-                int sum2 = totalSum-j;
-                ans = min(ans,abs(sum1-sum2));
-            }
-        }
-        
-        return ans;
+  int dfs(int arr[], int n, int totalSum , int i, int currSum, vector<vector<int>> &dp)
+  {
+      if(i >= n)
+      {
+          return abs(totalSum - 2*currSum);
+      }
+      
+      if(dp[i][currSum] != -1) return dp[i][currSum];
+      
+      int incl = dfs(arr, n, totalSum, i+1, currSum + arr[i], dp);
+      int excl = dfs(arr, n, totalSum, i+1, currSum, dp);
+      
+      return dp[i][currSum] = min(incl, excl);
+  }
+	int minDifference(int arr[], int n)  { 
+	    // Your code goes here
+	    int totalSum = 0;
+	    for(int i = 0; i < n; i++)
+	       totalSum += arr[i];
+	       
+	    vector<vector<int>> dp(n+1, vector<int>(totalSum + 1, -1));
+	    
+	    return dfs(arr, n, totalSum, 0, 0, dp);
 	} 
 };
 
