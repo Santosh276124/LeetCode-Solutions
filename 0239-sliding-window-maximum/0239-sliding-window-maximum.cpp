ceea24b220a76066223450adac28ba6d5e675e04
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2022-11-17 08:42:27 +0530

    Time: 374 ms (83.96%), Space: 134.6 MB (81.00%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -2,25 +2,62 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         
-        priority_queue<pair<int,int>> pq;
-        vector<int> ans;
-        int n = nums.size();
+//         priority_queue<pair<int,int>> pq;  //element, index;
+//         vector<int> ans;
+//         int n = nums.size();
         
-        for(int i = 0; i < k; i++)
-            pq.push({nums[i], i});
+//         for(int i = 0; i < k; i++)
+//             pq.push({nums[i], i});
         
-        ans.push_back(pq.top().first);
+//         ans.push_back(pq.top().first);
         
-        for(int i = k; i < n; i++)
-        {
-            pq.push({nums[i], i});
+//         for(int i = k; i < n; i++)
+//         {
+//             pq.push({nums[i], i});
             
-                while(pq.top().second <= (i-k))
-                    pq.pop();
+//                 while(pq.top().second <= (i-k))
+//                     pq.pop();
         
-            ans.push_back(pq.top().first);
+//             ans.push_back(pq.top().first);
+//         }
+        
+//         return ans;
+        
+        
+         deque<int> dq;
+       vector<int> ans;
+       int i = 0;
+       int j = 0;
+        
+        while(i < nums.size())
+        {
+            while(dq.size() && dq.back() < nums[i])
+            {
+                dq.pop_back();
+            }
+            
+            dq.push_back(nums[i]);
+            
+            if(i-j+1 < k)
+            {
+                // i++;
+            }
+            
+            else if(i-j+1 == k)
+            {
+               ans.push_back(dq.front());
+                
+                if(dq.front() == nums[j])
+                {
+                    dq.pop_front();
+                }
+                // i++;
+                j++;
+            }
+            
+            i++;
         }
-        
         return ans;
+        
     }
 };
