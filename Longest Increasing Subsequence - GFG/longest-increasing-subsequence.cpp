d52efa9d3bab6e862e47b87189b8f0a5f24fbc67
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2022-11-24 08:40:36 +0530

    Added solution - LeetHub

---
+++ Longest Increasing Subsequence - GFG/longest-increasing-subsequence.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    int solve(int ind, int prevInd, int n, int a[])
+    {
+       //base case
+      if(ind == n) return 0;
+       
+      int excl = 0 + solve(ind+1, prevInd, n, a);
+      int incl = 0;
+      if(prevInd == -1 || ( a[ind] > a[prevInd]) )
+            incl = 1 + solve(ind+1, ind, n, a);
+            
+      return max(excl, incl);
+    }
+    
+    int solveMem(int ind, int prevInd, int n, int a[], vector<vector<int>> &dp)
+    {
+       //base case
+      if(ind == n) return 0;
+      
+      if(dp[ind][prevInd+1] != -1) return dp[ind][prevInd+1];
+       
+      int excl = 0 + solveMem(ind+1, prevInd, n, a, dp);
+      int incl = 0;
+      if(prevInd == -1 || a[ind] > a[prevInd] )
+            incl = 1 + solveMem(ind+1, ind, n, a, dp);
+            
+      return dp[ind][prevInd+1] = max(excl, incl);
+    }
+    public:
+    //Function to find length of longest increasing subsequence.
+    int longestSubsequence(int n, int a[])
+    {
+       // your code here
+    //   return solve(0, -1, n, a);
+    
+        vector<vector<int>> dp(n, vector<int>(n+1,-1));
+        return solveMem(0, -1, n, a, dp);
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    //taking total testcases
+    int t,n;
+    cin>>t;
+    while(t--)
+    {
+        //taking size of array
+        cin>>n;
+        int a[n];
+        
+        //inserting elements to the array
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        Solution ob;
+        //calling method longestSubsequence()
+        cout << ob.longestSubsequence(n, a) << endl;
+    }
+}
+
+// } Driver Code Ends
