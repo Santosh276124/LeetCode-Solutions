f9a3029deed5f9034a1fe026f445829e8e28b2ba
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2022-11-24 11:02:18 +0530

    Added solution - LeetHub

--- Longest Increasing Subsequence - GFG/longest-increasing-subsequence.cpp
+++ Longest Increasing Subsequence - GFG/longest-increasing-subsequence.cpp
@@ -87,7 +87,33 @@ class Solution
         
         // return solveTab(n, a);
         
-        return solveSpace(n, a);
+        // return solveSpace(n, a);
+        
+        //approach 5;
+        vector<int> dp(n, 1);
+        int maxi = 1;
+        
+        for(int i = 0; i < n; i++)
+        {
+            for(int prev = 0; prev < i; prev++)
+            {
+                if(a[prev] < a[i])
+                {
+                    dp[i] = max(dp[i], 1 + dp[prev]);
+                }
+            }
+            maxi = max(maxi, dp[i]);
+        }
+        return maxi;
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 };
 
