daed4ebc2df551ce6479b7fcbe037a77b617c3b4
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2022-11-25 20:33:11 +0530

    Time: 57 ms (63.17%), Space: 6.5 MB (87.21%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -34,6 +34,28 @@ class Solution {
         }
        return (int)dp[n][m];     
     }
+    
+    int solveSpace(string &s, string t)
+    {
+        int n = s.length();
+        int m = t.length();
+       vector<double> prev(m+1, 0), curr(m+1, 0);
+        
+       prev[0] = curr[0] = 1;
+        
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <=m; j++)
+            {
+                if(s[i-1] == t[j-1])
+                     curr[j] = prev[j-1] + prev[j];
+                else
+                     curr[j] = prev[j];
+            }
+            prev = curr;
+        }
+       return (int)curr[m];     
+    }
 public:
     int numDistinct(string s, string t) {
         
@@ -42,6 +64,8 @@ public:
         // vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
         // return solve(n-1, m-1, s, t, dp);
         
-        return solveTab(s, t);
+        // return solveTab(s, t);
+        
+        return solveSpace(s, t);
     }
 };
