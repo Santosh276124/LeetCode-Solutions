07347f143fc35ef558280a37090b38b5119023dc
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2022-11-28 20:53:48 +0530

    Time: 1500 ms (32.94%), Space: 195.5 MB (18.91%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -3,42 +3,35 @@ public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
         
         unordered_map<int,int> lost;
-        unordered_map<int,int> win;
+       
         for(auto l : matches)
         {
             lost[l[1]]++;
-            win[l[0]]++;
         }
         
         vector<vector<int>> ans;
-        set<int> first;
-        set<int> second;
+        vector<int> win;
+        vector<int> looser;
+        
         for(auto val : matches)
         {
-            if(lost.count(val[0]) == false && win.count(val[0]) == true)
-            {
-                first.insert(val[0]);
-            }
-            
             if(lost[val[1]] == 1)
             {
-                second.insert(val[1]);
+                looser.push_back(val[1]);
+            }
+            
+            if(lost.count(val[0]) == false)
+            {
+                win.push_back(val[0]);
+                lost[val[0]] = 2;
             }
         }
-        vector<int> v1;
-        vector<int> v2;
-        
-        for(auto s : first)
-        {
-            v1.push_back(s);
-        }
-        for(auto s : second)
-        {
-            v2.push_back(s);
-        }
-        
-        ans.push_back(v1);
-        ans.push_back(v2);
+       
+        sort(win.begin(), win.end());
+        sort(looser.begin(), looser.end());
+      
+        ans.push_back(win);
+        ans.push_back(looser);
         return ans;
     }
 };
