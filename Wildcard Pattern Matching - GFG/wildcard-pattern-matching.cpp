e0961fab5371765a7f45325136519384659e9f98
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2022-12-19 13:38:22 +0530

    Added solution - LeetHub

--- Wildcard Pattern Matching - GFG/wildcard-pattern-matching.cpp
+++ Wildcard Pattern Matching - GFG/wildcard-pattern-matching.cpp
@@ -53,15 +53,54 @@ class Solution{
         return dp[i][j] = false; 
         
     }
-  public:
-/*You are required to complete this method*/
-    int wildCard(string pattern,string str)
+    
+    bool Tab( string &pattern, string &str)
     {
         int n = pattern.length();
         int m = str.length();
         
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
-        return Mem(n, m, pattern, str, dp);
+        vector<vector<bool>>dp(n+1, vector<bool>(m+1, false));
+        dp[0][0] = true;
+        for(int j = 1; j <= m; j++) dp[0][j] = false;
+        for(int i = 1; i <= n; i++)
+        {
+            bool flag = true;
+            for(int k = 1; k <= i; k++)
+            {
+                if(pattern[k-1] != '*')
+                    flag = false;
+            }
+            dp[i][0] = flag;
+        }
+        
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= m; j++)
+            {
+                if(pattern[i-1] == str[j-1] || pattern[i-1] == '?')
+                   dp[i][j] = dp[i-1][j-1];
+                
+                else if(pattern[i-1] == '*')
+                   dp[i][j] = dp[i][j-1] | dp[i-1][j];
+                 
+                else dp[i][j] = false;
+            }
+        }
+        
+        return dp[n][m];
+        
+    }
+  public:
+/*You are required to complete this method*/
+    int wildCard(string pattern,string str)
+    {
+        // int n = pattern.length();
+        // int m = str.length();
+        
+        // vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        // return Mem(n, m, pattern, str, dp);
+        
+        return Tab(pattern, str);
     }
 };
 
