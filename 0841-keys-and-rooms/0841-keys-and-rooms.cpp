d078077ee3e6eb8b2ffd5cf74c9452e029c5ed11
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2022-12-20 19:39:20 +0530

    Time: 18 ms (56.98%), Space: 12 MB (5.17%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+     
+        unordered_map<int,vector<int>> adj;
+        int n = rooms.size();
+        for(int i = 0; i < rooms.size(); i++)
+        {
+            for(auto j : rooms[i])
+            {
+                adj[i].push_back(j);
+            }
+        }
+        
+        vector<bool> vis(n, false);
+        
+        queue<int> q;
+        q.push(0);
+        vis[0] = true;
+        
+        while(!q.empty())
+        {
+            auto top = q.front();
+            q.pop();
+            
+            for(auto neigh : adj[top])
+            {
+                if(vis[neigh] == false)
+                {
+                    vis[neigh] = true;
+                    q.push(neigh);
+                }
+            }
+        }
+        for(int i = 0; i < n; i++)
+        {
+            if(vis[i] == false) return false;
+        }
+        return true;
+    }
+};
