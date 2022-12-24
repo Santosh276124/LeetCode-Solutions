284e5cb7ab46b0e8edc6e70bb52d6ba048a4f715
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2022-12-24 16:36:11 +0530

    Time: 4 ms (44.75%), Space: 6.5 MB (40.40%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
@@ -16,12 +16,37 @@ class Solution {
         
         return dp[n] = ((2*solveMem(n-1, dp)%mod)%mod + solveMem(n-3, dp)%mod)%mod;
     }
+    
+    int solveTab(int n)
+    {
+     
+        vector<int> dp(n+2, 0);
+        
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for(int i = 3; i <= n; i++)
+        {
+            
+            dp[i] = (2*dp[i-1]%mod + dp[i-3]%mod)%mod;
+            
+            // dp[i]%=mod;
+        }
+        
+        
+        
+        return dp[n]%mod;
+        // return 0;
+    }
 public:
     int numTilings(int n) {
         
         
         // return solve(n);
-        vector<int> dp(n+1, -1);
-        return solveMem(n, dp);
+        // vector<int> dp(n+1, -1);
+        // return solveMem(n, dp);
+        
+        return solveTab(n);
     }
 };
