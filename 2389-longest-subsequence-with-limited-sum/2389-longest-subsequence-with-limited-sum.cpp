f26c34e1b9ed8062f5199afd778628ca05d1622c
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2022-12-25 15:27:20 +0530

    Time: 99 ms (22.90%), Space: 13.8 MB (36.20%) - LeetHub

---
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
@@ -0,0 +1,43 @@
+class Solution {
+//     int dfs(int i, vector<int> &nums, int tar)
+//     {
+//         if(tar == 0) return 1;
+        
+//         if(i == 0) 
+//         {
+//             if(nums[0] <= tar) return 1;
+//             else return 0;
+//         }
+        
+//         int nottake = dfs(i-1, nums, tar);
+//         int take = 0;
+//         if(nums[i] <= tar)
+//             take = 1 + dfs(i-1, nums, tar-nums[i]);
+        
+//         return max(take , nottake);
+//     }
+public:
+    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+        
+        int n = nums.size();
+        int m = queries.size();
+        
+        sort(nums.begin(), nums.end());
+        vector<int> ans;
+        
+        for(auto tar : queries)
+        {
+            int cnt = 0;
+            for(int i = 0; i < n; i++)
+            {
+                if(nums[i] <= tar)
+                {
+                    tar = tar-nums[i];
+                    cnt++;
+                }
+            }
+            ans.push_back(cnt);
+        }
+        return ans;
+    }
+};
