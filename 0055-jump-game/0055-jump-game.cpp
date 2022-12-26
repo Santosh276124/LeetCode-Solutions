7067ff76f689658f582796e4e9acfabec011c037
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2022-12-26 09:47:37 +0530

    Time: 90 ms (74.53%), Space: 48.4 MB (79.88%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,48 +1,16 @@
 class Solution {
-    bool rec(int ind, vector<int> &nums, int n)
-    {
-        if(ind >= n) return false;
-        
-        if(ind == n-1) return true;
-        
-        int reachable = ind + nums[ind];
-        
-        for(int i = ind+1; i <= reachable; i++)
-        {
-            if(rec(i, nums, n))
-                return true;
-        }
-        
-      return false;  
-        
-    }
-    
-     bool Mem(int ind, vector<int> &nums, int n, vector<int> &dp)
-    {
-        if(ind >= n) return false;
-        
-        if(ind == n-1) return true;
-         
-         if(dp[ind] != -1) return dp[ind];
-        
-        int reachable = ind + nums[ind];
-        
-        for(int i = ind+1; i <= reachable; i++)
-        {
-            if(Mem(i, nums, n, dp))
-                return dp[ind] = true;
-        }
-        
-      return dp[ind] = false;  
-        
-    }
 public:
     bool canJump(vector<int>& nums) {
         
         int n = nums.size();
-        // return rec(0, nums, n);
+        int lastInd = n-1;
         
-        vector<int> dp(n+1, -1);
-        return Mem(0, nums, n, dp);
+        for(int i = n-1; i >= 0; i--)
+        {
+            if(nums[i] > 0 && i + nums[i] >= lastInd)
+                lastInd = i;
+        }
+        
+        return lastInd == 0;
     }
 };
