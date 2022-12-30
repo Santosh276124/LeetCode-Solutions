c317dc4aed13cdd6926ec2e90235356449b4abff
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2022-12-30 15:11:16 +0530

    Time: 14 ms (87.12%), Space: 12 MB (51.96%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -1,55 +1,31 @@
 class Solution {
     vector<vector<int>> ans;
     
-    void dfs(int node, int tar, unordered_map<int,vector<int> >&adj, vector<bool> &vis, vector<int> &temp)
+    void dfs(int node, int tar, vector<int> &temp, vector<vector<int>>& graph)
     {
+        temp.push_back(node);
+        
         if(node == tar)
         {
             ans.push_back(temp);
             return;
         }
         
-        if(!vis[node])
-            temp.push_back(node);
-        
-        vis[node] = true;
-        
-        for(auto neigh : adj[node])
+        for(auto neigh : graph[node])
         {
-            if(!vis[neigh])
-            {
-                vis[neigh] = true;
-                temp.push_back(neigh);
-                
-                dfs(neigh, tar, adj, vis, temp);
-                
-                vis[neigh] = false;
-                temp.pop_back();
-            }
-        }
-        return;
+            dfs(neigh, tar, temp, graph);
+            temp.pop_back();
+        }   
     }
 public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        
         int n = graph.size();
-        unordered_map<int,vector<int> > adj;
         
-        for(int i = 0; i < graph.size(); i++)
-        {
-            vector<int> u = graph[i];
-            
-            adj[i] = u;
-        }
-        
-        int source = 0;
         int tar = n-1;
-        
-        vector<bool> vis(n, false);
         vector<int> temp;
-        dfs(0, tar, adj, vis, temp);
-        
-        
-        
+        dfs(0, tar, temp, graph);
+
         return ans;
     }
 };
