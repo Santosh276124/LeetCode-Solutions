84354932c4bd91266358a1c423a8f40eb475ab11
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2022-12-30 16:51:48 +0530

    Time: 634 ms (92.80%), Space: 129.4 MB (69.81%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
@@ -2,36 +2,50 @@ class Solution {
 public:
     vector<int> getOrder(vector<vector<int>>& tasks) {
         
-         int n = tasks.size();
-        for(int i=0; i<n; i++) tasks[i].push_back(i); //indexing all the tasks so they dont loose their identity
-        sort(tasks.begin(),tasks.end());
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq; //min heap
+        int n = tasks.size();
+        for(int i = 0; i < n; i++){
+            tasks[i].push_back(i);
+            // cout<<tasks[i]<<" ";
+        }
+        
+        sort(tasks.begin(), tasks.end());
+        
+        priority_queue<pair<int,int>, vector<pair<int,int>> , greater<pair<int,int>> >pq;
+        
+        int ind = 0;
+        long long currTime = tasks[ind][0];
+        
         vector<int> ans;
         
-        int i =0;
-        long long t = tasks[0][0]; // current time
-        
-        while(!pq.empty() or i<n)
+        while(ind < n || !pq.empty())
         {
-            while(i<n && t>=tasks[i][0]) //keep pushing tasks into queue while the enqueueing time of next task is lower than the current time!!
-             {
-                pq.push({tasks[i][1],tasks[i][2]});
-                i++;
-             }
-           
-            if(pq.empty()) //if queue is empty and still the outer loop is running...then this means we still have tasks and also the queue is empty ---> CPU is idle from 't'(current time) t0 tasks[i][0](next enqueueing time)!!
+//             if(pq.empty() && ind < n)
+//             {
+                
+//             }
+            
+            
+            while(ind < n && tasks[ind][0] <= currTime)
             {
-                t = tasks[i][0];
+                pq.push({tasks[ind][1], tasks[ind][2]});
+                ind++;
             }
             
-            else  //queue is not empty and also the next enqueueing time is greater than curr time!! that means we do the task having shortest burst time untill our current time is equal of greater than the enqueueing time of next task
+            if(pq.empty())
             {
-               pair<int,int> p = pq.top();
-               pq.pop();
-               ans.push_back(p.second);
-               t += p.first; 
-            }  
+                currTime = tasks[ind][0];
+            }
+           else
+           {
+                pair<int,int> el = pq.top();
+                pq.pop();
+
+                currTime += el.first;
+
+                ans.push_back(el.second);
+           }
         }
+        
         return ans;
     }
 };
