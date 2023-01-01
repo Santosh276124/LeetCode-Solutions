c5ed425768c462cafa85a2a5bd491bae54a09bf7
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2023-01-01 18:01:31 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (19.87%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -1,68 +1,49 @@
-class Solution
-{
-    public:
-        bool wordPattern(string pattern, string s)
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        
+        vector<string> st;
+        
+        string temp = "";
+        for(int i = 0; i < s.length(); i++)
         {
-
-            unordered_map<string, vector <int> > ms;
-
-            string temp = "";
-            int ind = 0;
-            for (int i = 0; i < s.length(); i++)
+            if(s[i] == ' ')
             {
-                if (s[i] == ' ')
-                {
-                    ms[temp].push_back(ind);
-                    temp = "";
-                    ind++;
-                    continue;
-                }
-
-                temp += s[i];
-            }
-
-            ms[temp].push_back(ind);
-
-            unordered_map<string, vector <int> > mp;
-            temp = "";
-            for (int i = 0; i < pattern.length(); i++)
-            {
-                temp += pattern[i];
-                // cout<<temp<<" ";
-                mp[temp].push_back(i);
+                st.push_back(temp);
                 temp = "";
             }
+            else
+                temp += s[i];
             
-            for(auto m : mp)
-            {
-                cout<< m.first <<" ";
-            }
-             for(auto m : ms)
-            {
-                cout<< m.first <<" ";
-            }
-
-            if (ms.size() != mp.size()) return false;
-            
-            vector<vector<int>> v1;
-            vector<vector<int>> v2;
-            
-            for(auto m : mp)
-            {
-                sort(m.second.begin(), m.second.end());
-                v1.push_back(m.second);
-            }
-            for(auto m : ms)
-            {
-                sort(m.second.begin(), m.second.end());
-                v2.push_back(m.second);
-            }
-            
-            sort(v1.begin(), v1.end());
-            sort(v2.begin(), v2.end());
-            
-            return v1==v2;
-
-            // return true;
         }
+        
+        st.push_back(temp);
+        
+        if(pattern.size() != st.size()) return false;
+        
+        unordered_map<string,char> mp;
+        unordered_map<char,bool> vis;
+        
+        for(int i = 0; i < st.size(); i++)
+        {
+            if(mp.count(st[i]) == false && vis[pattern[i]] == true)
+            {
+                    return false;
+            }
+            else if(mp.count(st[i]))
+            {
+                if(mp[st[i]] != pattern[i]) return false;
+            }
+            else{
+                vis[pattern[i]] = true;
+                mp[st[i]] = pattern[i];
+            }
+               
+            
+        }
+        
+        return true;
+        
+        
+    }
 };
