ae66b6d76e3ee19f757ffcb1c9089145a68e72bb
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2023-01-04 12:50:55 +0530

    Time: 226 ms (70.79%), Space: 57.7 MB (54.44%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -1,37 +1,64 @@
 class Solution {
 public:
+    int n;
+    
+    int findNextInd(vector<vector<int>> &arr, int currInd, int tar)
+    {
+        int s = currInd + 1;
+        int e = n-1;
+        int ind = n+1;
+        while(s <= e)
+        {
+            int mid = s + (e-s)/2;
+            
+            if(arr[mid][0] >= tar)
+            {
+                ind = mid;
+                e = mid-1;
+            }
+            else
+                s = mid+1;
+        }
+        return ind;
+    }
+    
+    int max_profit(vector<vector<int>> &arr, int currInd, vector<int> &dp)
+    {
+        //base case
+        if(currInd >= n) return 0;
+        
+        if(dp[currInd] != -1) return dp[currInd];
+        
+        //
+        int notTake = 0 + max_profit(arr, currInd + 1, dp);
+        
+        
+        //
+        int nextInd = findNextInd(arr, currInd, arr[currInd][1]);
+        
+        int take = arr[currInd][2] + max_profit(arr, nextInd, dp);
+        
+        //
+        return dp[currInd] = max(notTake, take);
+    }
+    
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
         
-       vector<pair<int,pair<int,int>> > events;
-        for(int i = 0; i < profit.size(); i++)
+        n = startTime.size();
+        vector<vector<int>> arr(n, vector<int>(3, 0));
+        
+        for(int i = 0; i < n; i++)
         {
-            events.push_back({startTime[i], {endTime[i], profit[i]}});
+            arr[i][0] = startTime[i];
+            arr[i][1] = endTime[i];
+            arr[i][2] = profit[i];
         }
         
-        int ans = 0;
-        int maxVal = 0;
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>> > pq;
-        sort(events.begin(), events.end());
+        //sort
+        sort(arr.begin(), arr.end());
         
-        for(auto &e : events)
-        {
-            while(!pq.empty() && pq.top().first <= e.first)
-            {
-                maxVal = max(maxVal , pq.top().second);
-                pq.pop();
-            }
-            
-           
-            pq.push({e.second.first, maxVal+e.second.second});
-        }
+        vector<int> dp(n+1, -1);
         
-        while(!pq.empty())
-        {
-            maxVal = max(maxVal, pq.top().second);
-            pq.pop();
-           
-        }
-        return maxVal;
-       
+        return max_profit(arr, 0, dp);
     }
 };
