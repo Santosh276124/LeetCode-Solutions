4355e216e9ff7461e52bbe73196c6861e62ba055
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2023-01-11 12:55:22 +0530

    Time: 174 ms (95.30%), Space: 60.5 MB (83.89%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
-     int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
-        vector<vector<int>>graph(n);
-        unordered_set<int>v;
-        for(auto e:edges)
-        {
-            graph[e[0]].push_back(e[1]);
-            graph[e[1]].push_back(e[0]);
-        }
-        v.insert(0);
-        return dfs(graph, v, hasApple, 0);
-    }
-    int dfs(vector<vector<int>>& graph, unordered_set<int>& v, vector<bool>& has, int cur)
-    {
-        int res = 0;
-        for(auto a:graph[cur])
-        {
-            if(v.count(a))continue;
-            v.insert(a);
-            res += dfs(graph, v, has, a);
-        }
-        if((res > 0 || has[cur]) && cur != 0)res += 2;
-        return res;
+    vector<vector<int>> adjList;
+    vector<bool> visited;
+    
+    // returned value: rode length from root to and only to ALL apples which can be found beyond and include node
+    int dfs(int node,vector<bool>& hasApple,int d){
+        visited[node]=true;
+        int result=d;	//Initialize result as d.        
+        for(auto &child:adjList[node])
+            if(!visited[child]) {
+                int dist2apple=dfs(child,hasApple,d+1); //rode distance between root to ALL apples  beyound node 'child'
+                if(dist2apple)  //If child has apples it'll return a non zero result which is the distance traveled upto that node.
+                    result+=dist2apple-d;                     
+            }
         
+        return (hasApple[node]   // current 'node' has apple
+                ||               // or  
+                result > d) ?    // offspring(s) of current 'node' have apple(s)
+                result: 0;       // if true, we count the road length, otherwise, no road is needed.
+        
+    }
+    int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple){
+        adjList.resize(n);
+        visited.resize(n,false);
+        for(auto& e:edges)
+            adjList[e[0]].push_back(e[1]),adjList[e[1]].push_back(e[0]);
+        return dfs(0,hasApple,0)*2; 
     }
 };
