dcc1e5e12180bb883ee7bab81aa0af9ccf2dbda1
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2023-01-14 13:13:53 +0530

    Time: 1019 ms (30.34%), Space: 328.3 MB (5.10%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
@@ -1,59 +1,53 @@
 class Solution {
 public:
-    vector<int>tree[100005];
-    int ans;
-    int FindLongestLength(int curr_node,string&s){
+    int maxLen;
+    
+    int dfs(int node, int parent, unordered_map<int,list<int>>& adj, string &s)
+    {
+        int longest = 0;
+        int second_longest = 0;
         
-        int best=0,second_best=0;
-        
-        
-        // traversing over all the child nodes of the curr_node
-        for(auto child_node:tree[curr_node]){
-		    // recursively trying for child nodes
-            int len=FindLongestLength(child_node,s);
+        for(auto child : adj[node])
+        {
+            if(child == parent) continue;
             
-            // rejecting the current node if it's of same character
-            if(s[child_node]==s[curr_node])
-                continue;
+            int child_longest_path = dfs(child, node, adj, s);
             
-			
-			// updating the best and second_best paths that we can take from all the children of the given node
-            if(len>best){
-                second_best=best;
-                best=len;
-            }
-            else if(len>second_best) {
-                second_best=len;
-            }
+            if(s[child] == s[node]) continue;
+            
+            if(child_longest_path > second_longest)
+                second_longest = child_longest_path;
+            
+            if(second_longest > longest)
+                swap(longest, second_longest);
         }
         
-		
-	
-        // now there are 3 possibilies  as discussed
-		
-		// however here we have reject one of the possibility as we have already selcted the
-		// max from both the values
+        int neeche_hi_answer_mil_gya = 1 + longest + second_longest;
         
-        // case 1 :  selecting the max of the two possible paths 
-        int case1=1+best;
-		
-		// case 2:  
-        int case2=1+best+second_best;
+        int koi_ek_acha = 1 + max(longest, second_longest);
         
-        // updating the answer
-        ans=max({ans,case1,case2});
-		
-		
-        return case1;
+        int sirf_root_acha = 1;
         
+        maxLen = max({maxLen, neeche_hi_answer_mil_gya, koi_ek_acha, sirf_root_acha});
         
+        return max(koi_ek_acha, sirf_root_acha);
     }
     int longestPath(vector<int>& parent, string s) {
-        for(int i=1;i<parent.size();i++)
-            tree[parent[i]].push_back(i);
         
-        ans=0;
-        FindLongestLength(0,s);
-        return ans;
+        maxLen = 0;
+        unordered_map<int,list<int>> adj;
+        
+        for(int i = 1; i < parent.size(); i++)
+        {
+            int u = i;
+            int v = parent[i];
+            
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        
+        dfs(0, -1, adj, s);
+        
+        return maxLen;
     }
 };
