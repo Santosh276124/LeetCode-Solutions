b4cabf802b7c7dac01cc8f16a8826486c0a3c49a
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2023-01-20 08:41:48 +0530

    Added solution - LeetHub

---
+++ Geek Jump - GFG/geek-jump.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+  int solve(int ind, vector<int> &height, vector<int> &dp)
+  {
+      if(ind  < 0) return 0;
+      
+      if(ind == 0) return 0;
+      
+      if(dp[ind] != -1) return dp[ind];
+      
+      int one = INT_MAX;
+      if(ind-1 >= 0)
+       one = abs(height[ind]-height[ind-1])+ solve(ind-1, height, dp);
+      
+      int two = INT_MAX;
+      if(ind-2 >= 0)
+       two = abs(height[ind]-height[ind-2])+solve(ind-2, height, dp);
+      
+      return dp[ind] = min(one, two);
+      
+      
+  }
+    int minimumEnergy(vector<int>& height, int n) {
+        
+        // Code here
+        
+        vector<int> dp(n+1, -1);
+        
+        return solve(n-1 , height, dp);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<int> arr(N);
+        for (int i = 0; i < N; i++) {
+            cin >> arr[i];
+        }
+        Solution obj;
+        cout << obj.minimumEnergy(arr, N) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
