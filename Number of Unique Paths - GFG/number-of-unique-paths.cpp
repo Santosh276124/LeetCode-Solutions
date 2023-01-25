44ae9c46f8b475bd22aedf3f8ad4894dda640ab0
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2023-01-25 19:05:09 +0530

    Added solution - LeetHub

--- Number of Unique Paths - GFG/number-of-unique-paths.cpp
+++ Number of Unique Paths - GFG/number-of-unique-paths.cpp
@@ -95,7 +95,20 @@ class Solution
         // vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
         // return Mem(n-1, m-1, n, m, dp);
         
-        return space(n, m);
+        // return space(n, m);
+        
+        
+        // another approach
+        vector<vector<int>> dp(n, vector<int>(m, 1));
+        
+        for(int i = 1; i < n; i++)
+        {
+            for(int j = 1; j < m; j++)
+            {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[n-1][m-1];
     }
 };
 
