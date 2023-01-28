da4778616c18f7de57cfa98470114d58ff4a1ce5
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2023-01-28 12:17:45 +0530

    Added solution - LeetHub

--- Buy and Sell a Share at most twice - GFG/buy-and-sell-a-share-at-most-twice.cpp
+++ Buy and Sell a Share at most twice - GFG/buy-and-sell-a-share-at-most-twice.cpp
@@ -22,12 +22,57 @@ int f(int ind, int buy, int cap, vector<int> &prices, int n, vector<vector<vecto
     return dp[ind][buy][cap] = max(prices[ind] + f(ind+1, 1, cap-1, prices, n, dp), 
                 0 + f(ind+1, 0, cap, prices, n , dp));
 }
+int Tab(vector<int> &prices, int n)
+{
+    vector<vector<vector<int >>> dp(n+1 ,vector<vector<int>>(2, vector<int>(3, 0)));
+    
+    for(int ind = 0; ind < n; ind++)
+    {
+        dp[ind][0][0] = 0;
+        dp[ind][1][0] = 0;
+    }
+    for(int buy = 0; buy <= 1; buy++)
+    {
+        for(int cap = 0; cap <= 2; cap++)
+        {
+            dp[n][buy][cap] = 0;
+        }
+    }
+    
+    
+    
+    for(int ind = n-1; ind >= 0; ind--)
+    {
+        for(int buy = 0; buy <= 1; buy++)
+        {
+            for(int cap = 1; cap <= 2; cap++)
+            {
+               
+                if(buy)
+                {
+                   dp[ind][buy][cap] = max(-prices[ind] + dp[ind+1][0][cap] , 
+                                0 + dp[ind+1][1][cap] );
+                }
+            
+                else{
+                    
+                   dp[ind][buy][cap] = max(prices[ind] + dp[ind+1][1][cap-1] , 
+                            0 + dp[ind+1][0][cap] );
+                }
+            }
+        }
+    }
+    
+    return dp[0][1][2];
+}
 
 int maxProfit(vector<int>&price){
    
    int n = price.size();
-   vector<vector<vector<int >>> dp(n,vector<vector<int>>(2, vector<int>(3, -1)));
-    return f(0, 1, 2, price, n, dp);
+//   vector<vector<vector<int >>> dp(n,vector<vector<int>>(2, vector<int>(3, -1)));
+//     return f(0, 1, 2, price, n, dp);
+
+     return Tab(price, n);
    
 }
 
