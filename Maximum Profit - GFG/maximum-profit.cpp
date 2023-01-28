74ac02289c339de581dd6a039ea09869f0183983
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2023-01-28 13:03:57 +0530

    Added solution - LeetHub

--- Maximum Profit - GFG/maximum-profit.cpp
+++ Maximum Profit - GFG/maximum-profit.cpp
@@ -24,12 +24,65 @@ class Solution {
       return dp[ind][transaction] = max( a[ind] + solve(ind + 1, transaction+1, k, n, a, dp), 
                                          0 + solve(ind + 1, transaction, k, n, a, dp));
   }
+  
+  int Tab(int k, int n, int a[])
+  {
+      vector<vector<int>> dp(n+1, vector<int>(2*k+1, 0));
+      
+      for(int ind = n-1; ind >= 0; ind--)
+      {
+          for(int transaction = 2*k-1; transaction >= 0; transaction--)
+          {
+              if(transaction % 2 == 0) // can buy
+              {
+                 dp[ind][transaction] = max( -a[ind] + dp[ind + 1][transaction+1] , 
+                                                     0 + dp[ind+1][transaction]);
+              }
+              else {
+                 dp[ind][transaction] = max( a[ind] + dp[ind + 1][transaction+1] , 
+                                         0 + dp[ind + 1][transaction] );
+              }
+          }
+      }
+      
+      return dp[0][0];
+      
+  }
+  
+  int Space(int k, int n, int a[])
+  {
+      vector<int> next(2*k+1, 0);
+      vector<int> curr(2*k+1, 0);
+      
+      for(int ind = n-1; ind >= 0; ind--)
+      {
+          for(int transaction = 2*k-1; transaction >= 0; transaction--)
+          {
+              if(transaction % 2 == 0) // can buy
+              {
+                curr[transaction] = max( -a[ind] +next[transaction+1] , 
+                                                     0 + next[transaction]);
+              }
+              else {
+                curr[transaction] = max( a[ind] + next[transaction+1] , 
+                                         0 + next[transaction] );
+              }
+          }
+          
+          next = curr;
+      }
+      
+      return curr[0];
+      
+  }
     int maxProfit(int k, int n, int A[]) {
         // code here    
         
-        vector<vector<int>> dp(n+1, vector<int>(2*k, -1));
+        // vector<vector<int>> dp(n+1, vector<int>(2*k, -1));
         
-        return solve(0, 0, k, n, A, dp);
+        // return solve(0, 0, k, n, A, dp);
+        
+        return Space(k, n, A);
     }
 };
 
