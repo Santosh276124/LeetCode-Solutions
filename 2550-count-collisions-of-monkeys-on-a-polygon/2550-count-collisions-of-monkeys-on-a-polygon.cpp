595fc5becdfc225273479b01375e40004fa0de39
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2023-01-29 11:37:36 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (75.00%) - LeetHub

--- 2550-count-collisions-of-monkeys-on-a-polygon/2550-count-collisions-of-monkeys-on-a-polygon.cpp
+++ 2550-count-collisions-of-monkeys-on-a-polygon/2550-count-collisions-of-monkeys-on-a-polygon.cpp
@@ -4,7 +4,7 @@ public:
         
         long res = 1, base = 2, mod = 1e9 + 7;
         while (n > 0) {
-            if (n % 2)
+            if (n % 2 == 1)
                 res = res * base % mod;
             base = base * base % mod;
             n >>= 1;
