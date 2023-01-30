ec54ae4dcf723a8aefc6345e9b68ef04c0df5f1f
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2023-01-30 07:58:53 +0530

    Time: 4 ms (74.41%), Space: 12.1 MB (13.12%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,15 +1,34 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int sold = 0, hold = INT_MIN, rest = 0;
-    for (int i=0; i<prices.size(); ++i)
+    int solve(int ind, int buy, int n, vector<int > &prices, vector<vector<int>>& dp)
     {
-        int prvSold = sold;
-        sold = hold + prices[i];
-        hold = max(hold, rest-prices[i]);
-        rest = max(rest, prvSold);
+        if(ind >= n) return 0;
+        
+        if(dp[ind][buy] != -1) return dp[ind][buy];
+        
+        if(buy)
+        {
+            return dp[ind][buy] = max(-prices[ind] + solve(ind+1, 0, n, prices, dp), solve(ind+1, 1, n, prices, dp));
+        }
+        
+        //if i am selling tehn i can not again buy the next day so ind+2;
+        return dp[ind][buy] = max(prices[ind] + solve(ind+2, 1, n, prices, dp), solve(ind+1, 0, n, prices, dp));
     }
-    return max(sold, rest);
+    int maxProfit(vector<int>& prices) {
+        
+        int n = prices.size();
+        vector<vector<int>> dp(n+1, vector<int>(2, -1));
+        return solve(0, 1, n, prices, dp);
+        
+    //     int sold = 0, hold = INT_MIN, rest = 0;
+    // for (int i=0; i<prices.size(); ++i)
+    // {
+    //     int prvSold = sold;
+    //     sold = hold + prices[i];
+    //     hold = max(hold, rest-prices[i]);
+    //     rest = max(rest, prvSold);
+    // }
+    // return max(sold, rest);
 
     }
 };
