3482aadd82a41f2fda6e10b1f069dc7bc19ffc8e
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2023-01-30 08:12:22 +0530

    Added solution - LeetHub

--- Buy Stock with Cooldown - GFG/buy-stock-with-cooldown.cpp
+++ Buy Stock with Cooldown - GFG/buy-stock-with-cooldown.cpp
@@ -40,13 +40,35 @@ class Solution{
         
         return dp[0][1];
     }
+    long long Space(int n, vector<long long > &prices)
+    {
+         vector<long long> front2(2, 0);
+         vector<long long> front1(2, 0);
+         vector<long long> curr(2, 0);
+        
+        for(int ind = n-1; ind >= 0; ind--)
+        {
+            
+            curr[1] = max(-prices[ind] + front1[0], front1[1]);
+                
+                
+                //if i am selling tehn i can not again buy the next day so ind+2;
+            curr[0] = max(prices[ind] + front2[1], front1[0]);
+            
+            front2 = front1;
+            front1 = curr;
+            
+        }
+        
+        return curr[1];
+    }
     long long maximumProfit(vector<long long>&prices, int n) {
         // Code here
         
         // vector<vector<long long>> dp(n+1, vector<long long>(2, -1));
         // return solve(0, 1, n, prices, dp);
         
-        return Tab(n, prices);
+        return Space(n, prices);
     }
 };
 
