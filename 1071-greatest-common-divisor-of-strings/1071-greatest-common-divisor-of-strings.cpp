b2ea31a57030ecbd0442272d5caa8eb334a811d5
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2023-02-01 20:36:55 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (65.71%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
+    int gcd(int a, int b){
+        if(b == 0) return a;
+        
+        return gcd(b, a%b);
+    }
     string gcdOfStrings(string str1, string str2) {
         
         int n = str1.length();
         int m = str2.length();
         
         // cout<<__gcd(n,m)<<" ";
-        string ans = str1.substr(0, __gcd(n,m));
+        string ans = str1.substr(0, gcd(n,m));
         
         return str1+str2 == str2+str1 ? ans : "";
     }
