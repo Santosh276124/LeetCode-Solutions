15ab650f11a6f96dd0df7b4ef09a3f641bc6a803
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2023-02-02 08:38:44 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
@@ -25,11 +25,41 @@ int solve(int i, int j, int arr[],  vector<vector<int>>& dp){
     
     return dp[i][j] = mini;
 }
+
+int solveTab(int arr[], int n){
+    vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+    
+    for(int i = n-1; i >= 1; i--)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            if(i == j) dp[i][j] = 0;
+            else
+            {
+                int mini = 1e9;
+                for(int k = i; k <= j-1; k++)
+                {
+                    int steps = 0;
+                    if(k+1 < n && k >= 0)
+                     steps = arr[i-1]*arr[k]*arr[j] + dp[i][k] + dp[k+1][j];
+                    
+                    mini = min(mini, steps);
+                }
+                
+                dp[i][j] = mini;
+            }
+        }
+    }
+    return dp[1][n-1];
+    
+}
     int matrixMultiplication(int n, int arr[])
     {
         // code here
-        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
-        return solve(1 ,n-1, arr, dp);
+        // vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        // return solve(1 ,n-1, arr, dp);
+        
+        return solveTab(arr, n);
     }
 };
 
