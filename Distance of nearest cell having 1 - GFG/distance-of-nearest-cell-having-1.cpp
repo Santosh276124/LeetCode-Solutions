8de21b6d91facf2cbc9e588293a273875c301b9c
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2023-03-09 08:17:39 +0530

    Added solution - LeetHub

--- Distance of nearest cell having 1 - GFG/distance-of-nearest-cell-having-1.cpp
+++ Distance of nearest cell having 1 - GFG/distance-of-nearest-cell-having-1.cpp
@@ -9,79 +9,56 @@ class Solution
     //Function to find distance of nearest 1 in the grid for each cell.
 	vector<vector<int>>nearest(vector<vector<int>>grid)
 	{
-	     int n =grid.size() ;
-
-  int m = grid[0].size(); 
-
-  vector<int>dr={-1 ,0 , 1 ,0 } ;
-
-  vector<int>dc = {0 , 1 , 0 , -1} ;
-
-  
-
-  vector<vector<int>>vis(n, vector<int>(m ,0 )) ;
-
-  vector<vector<int>>dis(n, vector<int>(m ,0 )) ;
-
-     
-
-     queue<pair<pair<int ,int > ,int >> q ;
-
-     for(int i=0 ;i<n ;i++ )
-
-     {
-
-         for(int j=0; j<m; j++ )
-
-         {
-
-             if(grid[i][j]==1)
-
-             q.push({{i ,j }, 0 }) ,vis[i][j]=1 ; 
-
-         }
-
-     }
-
-     
-
-     while(!q.empty())
-
-     {
-
-         int x = q.front().first.first ;
-
-         int y =q.front().first.second; 
-
-         int d = q.front().second ;
-
-         q.pop() ;
-
-         for(int k =0 ; k<4 ;k++ )
-
-         {
-
-             int nx = x+dr[k] ;
-
-             int ny= y+dc[k]; 
-
-             if(nx>=0  && nx<n && ny>=0 && ny<m && grid[nx][ny]==0 && !vis[nx][ny])
-
-             {
-
-                 dis[nx][ny] = d+1 ;
-
-                 q.push({{nx ,ny } ,d+1} ); 
-
-                 vis[nx][ny]=1;
-
-             }
-
-         }
-
-     }
-
-   return dis; 
+	    // Code here
+	    int n = grid.size();
+	    int m = grid[0].size();
+	    
+	    vector<vector<int>> vis(n, vector<int>(m, 0));
+	    vector<vector<int>> ans(n, vector<int>(m, 0));
+	    
+	    queue<pair< pair<int,int>, int> > q;
+	    
+	    int dx[] = {-1, 0, 1, 0};
+	    int dy[] = {0, 1, 0, -1};
+	    
+	    for(int i = 0; i < n; i++)
+	    {
+	        for(int j = 0; j < m; j++)
+	        {
+	            if(grid[i][j] == 1)
+	            {
+	                q.push({{i, j}, 0});
+	                vis[i][j] = 1;
+	            }
+	        }
+	        
+	    }
+	    
+	    while(!q.empty())
+	    {
+	        auto front = q.front();
+	        q.pop();
+	        int x = front.first.first;
+	        int y = front.first.second;
+	        int d = front.second;
+	        
+	        ans[x][y] = d;
+	        
+	        for(int i = 0; i < 4; i++)
+	        {
+	            int nx = x + dx[i];
+	            int ny = y + dy[i];
+	            
+	            if(nx >= 0 && nx < n && ny >=0 && ny < m && vis[nx][ny] == 0 && grid[nx][ny] == 0)
+	            {
+	                q.push({{nx, ny}, d+1});
+	                vis[nx][ny] = 1;
+	            }
+	        }
+	        
+	    }
+	    
+	    return ans;
 	}
 };
 
