5ac6af98e7a9d0153c4804a4df42e9585b830116
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2023-03-11 20:32:30 +0530

    Time: 8 ms (68.88%), Space: 15.1 MB (8.96%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,56 +10,45 @@
  */
 class Solution {
 public:
-    ListNode* merge(ListNode* list1, ListNode* list2)
-    {
-        if(list1->next == NULL){
-            list1->next = list2;
-            return list1;
-        }
-        
-        ListNode* c1 = list1;
-        ListNode* c2 = list2;
-        ListNode* n1 = c1->next;
-        ListNode* n2 = c2->next;
-        
-        while(n1 != NULL && c2 != NULL)
-        {
-            if(c2 -> val >= c1 -> val && c2->val <= n1->val)
-            {
-                c1->next = c2;
-                n2 = c2->next;
-                c2->next = n1;
-                c1 = c2;
-                c2 = n2;
-            }
-            else
-            {
-                c1 = n1;
-                n1 = n1->next;
-                if(n1 == NULL)
-                {
-                    c1->next = c2;
-                    return list1;
-                }
-            }
-        }
-        return list1;
-    }
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         
         if(list1 == NULL) return list2;
         if(list2 == NULL) return list1;
         
-        if(list1->val <= list2->val)
+        ListNode* ans = new ListNode(-1);
+        
+        ListNode* c1 = list1;
+        ListNode* c2 = list2;
+        ListNode* temp = ans;
+        
+        while(c1 != NULL && c2 != NULL)
         {
-            ListNode* ans = merge(list1, list2);
-            return ans;
-        }
-        else{
-            ListNode* ans = merge(list2, list1);
-            return ans;
+            if(c1->val <= c2->val)
+            {
+                temp->next = new ListNode(c1->val);
+                c1 = c1->next;
+                temp = temp->next;
+            }
+            else{
+                temp->next = new ListNode(c2->val);
+                c2 = c2->next;
+                temp = temp->next;
+            }
         }
         
-      
+        while(c1 != NULL)
+        {
+            temp->next = new ListNode(c1->val);
+            c1 = c1->next;
+            temp = temp->next;
+        }
+        while(c2 != NULL)
+        {
+            temp->next = new ListNode(c2->val);
+            c2 = c2->next;
+            temp = temp->next;
+        }
+        
+        return ans->next;
     }
 };
