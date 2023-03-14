c86e46a7094adafed46facb8629de5c9bf3136f6
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2023-03-14 19:58:59 +0530

    Added solution - LeetHub

--- Floyd Warshall - GFG/floyd-warshall.cpp
+++ Floyd Warshall - GFG/floyd-warshall.cpp
@@ -11,27 +11,44 @@ class Solution {
   public:
 	void shortest_distance(vector<vector<int>>&mat){
 	    // Code here
+	    
 	    int n = mat.size();
 	    
-	    
-	    
-	    for(int k = 0; k < n; k++)
+	    for(int i = 0; i < n; i++)
 	    {
-	        for(int i = 0; i < n; i++)
-	         {
-	             for(int j = 0; j < n; j++)
-	             {
-	                 if(i!=j && i != k && k  != j && mat[i][k] != -1 && mat[k][j] != -1) {
-	                     if(mat[i][j] == -1)
-	                      mat[i][j] = mat[i][k]+mat[k][j];
-	                     else
-	                      mat[i][j] = min(mat[i][j], mat[i][k]+mat[k][j]);
-	                  }
-	             }
-	         }
+	        for(int j = 0; j < n; j++)
+	        {
+	            if(mat[i][j] == -1)
+	                mat[i][j] = 1e9;
+	           
+	            if(i == j)
+	                mat[i][j] = 0;
+	        }
 	    }
 	    
-	     
+	    
+	    for(int via = 0; via < n; via++)
+	    {
+	        for(int i = 0; i < n; i++)
+    	    {
+    	        for(int j = 0; j < n; j++)
+    	        {
+    	            mat[i][j] = min(mat[i][j], mat[i][via] + mat[via][j]);
+    	        }
+    	    }
+	    }
+	    
+	    for(int i = 0; i < n; i++)
+	    {
+	        for(int j = 0; j < n; j++)
+	        {
+	            if(mat[i][j] == 1e9)
+	                mat[i][j] = -1;
+	           
+	           
+	        }
+	    }
+	    
 	}
 };
 
