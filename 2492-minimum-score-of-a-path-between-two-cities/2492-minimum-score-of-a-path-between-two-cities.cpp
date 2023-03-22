bcca474a1cddd73deab2fe531854acba37667fa2
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2023-03-22 13:50:49 +0530

    Time: 587 ms (33.70%), Space: 149.6 MB (16.31%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,3 +1,45 @@
+
+class DisjointSet {
+    vector<int> parent, rank;
+    public:
+        DisjointSet(int V){
+            parent.resize(V+1);
+            rank.resize(V+1);
+            for(int i = 0; i <= V; i++){
+                parent[i] = i;
+                rank[i] = 0;
+            }
+        }
+    
+    int findParent(int node){
+        if(node == parent[node]){
+            return node;
+        }
+        
+        return parent[node] = findParent(parent[node]);
+    }
+    
+    void unionByRank(int u, int v){
+        int ult_u = findParent(u);
+        int ult_v = findParent(v);
+        
+        if(ult_u == ult_v) return;
+        
+        if(rank[ult_u] < rank[ult_v]){
+            parent[ult_u] = ult_v;
+        }
+        else if(rank[ult_u] > rank[ult_v]){
+            parent[ult_v] = ult_u;
+        }
+        else{
+            parent[ult_v] = ult_u;
+            rank[ult_u]++;
+        }
+        
+        
+    }
+};
+
 class Solution {
 public:
     void dfs(int node, unordered_map<int, list<pair<int,int>> >& adj, vector<int> &vis, int &ans){
@@ -29,9 +71,35 @@ public:
         
         int ans = 1e9;
         
-        vector<int> vis(n+1, 0);
         
-        dfs(1, adj, vis, ans);
+        DisjointSet ds(n);
+        
+        for(int i = 0; i < roads.size(); i++){
+            int u = roads[i][0];
+            int v = roads[i][1];
+            
+            ds.unionByRank(u, v);
+        }
+        
+        int ult_par = ds.findParent(1);
+        
+        for(int i = 0; i < roads.size(); i++){
+            int u = roads[i][0];
+            int v = roads[i][1];
+            int d = roads[i][2];
+            
+            if(ds.findParent(u) == ult_par && ds.findParent(v) == ult_par){
+                ans = min(ans, d);
+            }
+    
+        }
+        
+        
+        
+        
+        // vector<int> vis(n+1, 0);
+        
+        // dfs(1, adj, vis, ans);
         
 //         queue<int> q;
         
