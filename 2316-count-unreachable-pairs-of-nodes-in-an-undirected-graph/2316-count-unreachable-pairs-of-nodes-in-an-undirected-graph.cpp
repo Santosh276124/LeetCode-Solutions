68022346793573b1c6a947b873ff3189324d7f6c
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2023-03-25 17:53:13 +0530

    Time: 919 ms (16.47%), Space: 158.7 MB (87.35%) - LeetHub

--- 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
@@ -1,62 +1,80 @@
-class Solution {
-public:
-    void dfs(int node,  unordered_map<int, list<int> >& adj, vector<int> &vis, long long &cnt){
+class DisjointSet {
+  public:
+    vector<int> parent, size, rank;
+    DisjointSet(int V){
+        parent.resize(V+1);
+        rank.resize(V+1, 0);
+        size.resize(V+1, 1);
+        for(int i = 0; i <= V; i++){
+            parent[i] = i;
+        }
+    }
+    
+    int findPar(int node){
+        if(node == parent[node])
+            return node;
         
-        vis[node] = 1;
-        cnt++;
+        return parent[node] = findPar(parent[node]);
+    }
+    
+    void unionBySize(int u, int v){
         
-        for(auto neigh : adj[node]){
-            if(!vis[neigh]){
-                dfs(neigh, adj, vis, cnt);
-            }
+        int ult_u = findPar(u);
+        int ult_v = findPar(v);
+        
+        if(ult_u == ult_v) return;
+        
+        if(rank[ult_u] < rank[ult_v]){
+            parent[ult_u] = ult_v;
+            size[ult_v] += size[ult_u];
+        } 
+        else{
+            parent[ult_v] = ult_u;
+            size[ult_u] += size[ult_v];
         }
         
     }
+};
+
+class Solution {
+public:
     long long countPairs(int n, vector<vector<int>>& edges) {
-          
-        unordered_map<int, list<int> > adj;
+        
+        DisjointSet ds(n);
+        
+        vector<int> temp;
+        long long totalSum = 0;
         
         for(auto it : edges){
-            adj[it[0]].push_back(it[1]);
-            adj[it[1]].push_back(it[0]);
+            int u = it[0];
+            int v = it[1];
+            
+            if(ds.findPar(u) == ds.findPar(v))
+                continue;
+            else
+                ds.unionBySize(u, v);
         }
         
         
-        
-        vector<int> vis(n, 0);
-        
-        vector<long long> temp;
         
         for(int i = 0; i < n; i++){
             
-            long long cnt = 0;
+            cout<< ds.parent[i] <<" ";
             
-            if(vis[i] == 0){
-                
-                dfs(i, adj, vis, cnt);
-      
+            if(ds.parent[i] == i){
+                totalSum += ds.size[i];
+                temp.push_back(ds.size[i]);
             }
-
-            temp.push_back(cnt);    
         }
-        
-        int len = temp.size();
         
         long long ans = 0;
-        long long totalSum = 0;
-        //prefix sum
-        for(int i = 0; i < len; i++){
-            totalSum += temp[i];
-        }
-        
-        for(int i = 0; i < n; i++){
-            
-            ans += ( temp[i] * (totalSum - temp[i]) );
+        for(int i = 0; i < temp.size(); i++){
+            ans += (temp[i] * (totalSum - temp[i]));
             
             totalSum -= temp[i];
-            
         }
         
         return ans;
+        
     }
 };
