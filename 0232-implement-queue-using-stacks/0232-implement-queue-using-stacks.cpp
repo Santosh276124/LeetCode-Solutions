fc2ca0e2c3e88181c48093bcf1aeeb585e239ec6
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2023-03-25 08:58:55 +0530

    Time: 2 ms (44.44%), Space: 7 MB (44.18%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -1,43 +1,78 @@
 class MyQueue {
 public:
-    stack<int> s1, s2;
+    // stack<int> s1, s2;
+    stack<int> stin, stout;
     MyQueue() {
         
     }
     
     void push(int x) {
         
-        //s1 to s2
-        while(!s1.empty()){
-            int top = s1.top();
-            s1.pop();
-            s2.push(top);
-        }
+//         //s1 to s2
+//         while(!s1.empty()){
+//             int top = s1.top();
+//             s1.pop();
+//             s2.push(top);
+//         }
         
-        s1.push(x);
+//         s1.push(x);
         
-        //s2 to s1
-        while(!s2.empty()){
-            int top = s2.top();
-            s2.pop();
-            s1.push(top);
-        }
+//         //s2 to s1
+//         while(!s2.empty()){
+//             int top = s2.top();
+//             s2.pop();
+//             s1.push(top);
+//         }
         
+        
+        stin.push(x);
         
     }
     
     int pop() {
-        int top = s1.top();
-        s1.pop();
-        return top;
+        if(!stout.empty()){
+            int top = stout.top();
+            stout.pop();
+            return top;
+        }
+        else{
+            while(!stin.empty()){
+                int top = stin.top();
+                stin.pop();
+                stout.push(top);
+            }
+            
+            int top = stout.top();
+            stout.pop();
+            return top;
+        }
     }
     
     int peek() {
-        return s1.top();
+        
+        if(!stout.empty()){
+            return stout.top();
+        }
+        else{
+            while(!stin.empty()){
+                int top = stin.top();
+                stin.pop();
+                stout.push(top);
+            }
+            return stout.top();
+        }
+        
     }
     
     bool empty() {
-        return s1.size() == 0;
+       
+            while(!stin.empty()){
+                int top = stin.top();
+                stin.pop();
+                stout.push(top);
+            }
+             return stout.size() == 0;
+
     }
 };
 
