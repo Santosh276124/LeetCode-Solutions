c2eb36d59630b7dc454ecb5650751d633048a16d
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2023-04-03 07:32:56 +0530

    Time: 262 ms (48.76%), Space: 99.9 MB (31.46%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -1,25 +1,44 @@
 class Solution {
 public:
-    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
-         int n = spells.size();
-        int m = potions.size();
-        vector<int> pairs(n, 0);
-        sort(potions.begin(), potions.end());
-        for (int i = 0; i < n; i++) {
-            int spell = spells[i];
-            int left = 0;
-            int right = m - 1;
-            while (left <= right) {
-                int mid = left + (right - left) / 2;
-                long long product = (long long)spell * (long long)potions[mid];
-                if (product >= success) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
-            }
-            pairs[i] = m - left;
+    int n;
+    int m;
+    int binarySearch(int curr , vector<int> &arr, long long tar){
+        
+        int s = 0;
+        int e = m-1;
+        
+        while(s <= e){
+            int mid = s  + (e-s)/2;
+            long long  prod = (arr[mid] * long(curr) );
+            if(prod >= tar)
+                e = mid-1;
+            
+            else
+                s = mid+1;
         }
-        return pairs;
+        
+        return m-s;
+        
+    }
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        
+        // sort(spells.begin(), spells.end());
+        sort(potions.begin(), potions.end());
+        
+         n = spells.size();
+         m = potions.size();
+        
+        vector<int> ans;
+        
+        for(int i = 0; i < n; i++){
+            
+            int val = binarySearch(spells[i], potions, success);
+            
+            ans.push_back(val);
+            
+        }
+        
+        return ans;
+        
     }
 };
