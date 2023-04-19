5aad82f5644c1f65223b6d6553e950026b26611d
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2023-04-19 19:49:23 +0530

    Time: 183 ms (43.55%), Space: 94.3 MB (63.41%) - LeetHub

--- 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.cpp
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.cpp
@@ -12,18 +12,40 @@
 class Solution {
 public:
     int maxLen = 0;
-    void solve(TreeNode* root, int l, int r){
+//     void solve(TreeNode* root, int l, int r){
+        
+//         if(root == NULL) return;
+        
+//         maxLen = max(maxLen, max(l, r));
+        
+//         solve(root->left, r+1, 0);
+        
+//         solve(root->right, 0, l+1);
+        
+//     }
+    
+    void solve(TreeNode* root, int fromLeft, int steps){
         
         if(root == NULL) return;
         
-        maxLen = max(maxLen, max(l, r));
+        maxLen  = max(maxLen, steps);
         
-        solve(root->left, r+1, 0);
-        
-        solve(root->right, 0, l+1);
+        if(fromLeft){
+            solve(root->right, !fromLeft, steps+1);
+            
+            solve(root->left, fromLeft, 1);
+        }
+        else{
+            solve(root->left, !fromLeft, steps+1);
+            
+            solve(root->right, fromLeft, 1);
+        }
         
     }
+    
     int longestZigZag(TreeNode* root) {
+        
+        // solve(root, 0, 0);
         
         solve(root, 0, 0);
         
