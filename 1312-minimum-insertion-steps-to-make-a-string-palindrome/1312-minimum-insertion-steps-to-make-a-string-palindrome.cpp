5549c0e97c85eb330ccc842e16d2b19ed5c8db1b
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2023-04-22 10:58:18 +0530

    Time: 69 ms (59.08%), Space: 6.8 MB (89.32%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -39,6 +39,30 @@ public:
         return dp[n][n];
         
     }
+    int LPSSpace(string &s1, string &s2){
+        
+        vector<int> prev(n+1, 0), curr(n+1, 0);
+        
+        for(int i = 0; i <= n; i++)
+            curr[0] = 0;
+        for(int j = 0; j <= n; j++)
+           prev[j] = 0;
+        
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                if(s1[i-1] == s2[j-1])
+                     curr[j] = 1 + prev[j-1];
+                
+                else
+                     curr[j] = max( prev[j] , curr[j-1] );
+            }
+            
+            prev = curr;
+        }
+        
+        return curr[n];
+        
+    }
     int minInsertions(string s) {
         
          n = s.length();
@@ -50,7 +74,7 @@ public:
         
         // int lps = LPS(n, n, s1, s, dp);
         
-        int lps = LPSTab(s1, s);
+        int lps = LPSSpace(s1, s);
         
         return n - lps;
         
