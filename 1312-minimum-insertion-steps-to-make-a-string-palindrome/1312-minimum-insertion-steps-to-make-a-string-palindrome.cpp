4bd1ab3024b01cd2762aecb382e2fa36ba32cfa4
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2023-04-22 10:55:59 +0530

    Time: 110 ms (22.76%), Space: 28.5 MB (50.59%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -17,13 +17,28 @@ public:
         
     }
     
-//     int LPSTab(string &s1, string &s2){
+    int LPSTab(string &s1, string &s2){
             
-//         vector<vector<int>> dp(n+1 , vector<int>(n+1, 0));
+        vector<vector<int>> dp(n+1 , vector<int>(n+1, 0));
         
+        for(int i = 0; i <= n; i++)
+            dp[i][0] = 0;
+        for(int j = 0; j <= n; j++)
+            dp[0][j] = 0;
         
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                if(s1[i-1] == s2[j-1])
+                     dp[i][j] = 1 + dp[i-1][j-1];
+                
+                else
+                     dp[i][j] = max( dp[i-1][j] , dp[i][j-1] );
+            }
+        }
         
-//     }
+        return dp[n][n];
+        
+    }
     int minInsertions(string s) {
         
          n = s.length();
@@ -31,9 +46,11 @@ public:
         
         reverse(s.begin(), s.end());
         
-        vector<vector<int>> dp(n+1 , vector<int>(n+1, -1));
+        // vector<vector<int>> dp(n+1 , vector<int>(n+1, -1));
         
-        int lps = LPS(n, n, s1, s, dp);
+        // int lps = LPS(n, n, s1, s, dp);
+        
+        int lps = LPSTab(s1, s);
         
         return n - lps;
         
