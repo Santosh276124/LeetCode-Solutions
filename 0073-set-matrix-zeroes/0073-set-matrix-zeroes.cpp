e1f46c16aa0b9e4d297307eb6f0dbdd9a233085b
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2023-04-26 20:06:33 +0530

    Time: 12 ms (79.05%), Space: 13.3 MB (78.44%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -2,35 +2,53 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         
-        set<pair<int,int>> st;
+    
         int n = matrix.size();
         int m = matrix[0].size();
         
+        bool isRow0 = 0;
+        bool isCol0 = 0;
+        
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(matrix[i][j] == 0){
-                    st.insert({i, j});
-                }
-            }
+            if(matrix[i][0] == 0)
+                isRow0 = 1;
         }
         
-        for(auto s : st){
-            // cout<<m<<" ";
-            int r = s.first;
-            int c = s.second;
-            
-            cout<<r <<" "<< c<<" ";
-            
-            //row to 0
-            for(int i = 0; i < m; i++){
-                matrix[r][i] = 0;
-            }
-            //column to 0
-            for(int i = 0; i < n; i++){
-                matrix[i][c] = 0;
-            }
+        for(int j = 0; j < m; j++){
+            if(matrix[0][j] == 0)
+                isCol0 = 1;
         }
         
+        for(int i = 1; i < n; i++){
+           for(int j = 1; j < m; j++){
+               if(matrix[i][j] == 0){
+                   matrix[i][0] = 0;
+                   matrix[0][j] = 0;
+               }
+           }
+        }
+        
+        for(int i = 1; i < n; i++){
+           for(int j = 1; j < m; j++){
+               if(matrix[0][j] == 0 || matrix[i][0] == 0){
+                   matrix[i][j] = 0;
+                  
+               }
+           }
+        }
+        
+        if(isCol0){
+            for(int j = 0; j < m; j++)
+                matrix[0][j] = 0;
+        }
+        
+        if(isRow0){
+            for(int i = 0; i < n; i++)
+                matrix[i][0] = 0;
+        }
+        
+        
+       
         return ;
         
     }
