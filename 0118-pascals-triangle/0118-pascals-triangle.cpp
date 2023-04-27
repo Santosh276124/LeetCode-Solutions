1f3b72004935bad8fbaf5369ffdec1ad1da5840f
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2023-04-27 18:26:11 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (52.55%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -4,14 +4,20 @@ public:
         
         vector<vector<int>> ans;
         
-        for(int i = 0; i < n; i++){
-            vector<int> temp(i+1, 1);
+        // ans[0].push_back({1});
+        // ans[1].push_back({1, 1});
+        
+        for(int i = 0; i < n; i++){       
             
+            vector<int> curr(i+1, 1);
+            
+            // ans.push_back(curr);
+       
             for(int j = 1; j < i; j++){
-                temp[j] = ans[i-1][j] + ans[i-1][j-1];
+                curr[j] = ans[i-1][j] + ans[i-1][j-1];
             }
             
-            ans.push_back(temp);
+            ans.push_back(curr);
         }
         
         return ans;
