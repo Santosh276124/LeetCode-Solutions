3d9283b470fec62e38544ffb92173841806cb760
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2023-04-27 19:18:01 +0530

    Time: 5 ms (28.92%), Space: 6.7 MB (52.55%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,27 +1,39 @@
 class Solution {
 public:
+    long long NCR(long long n, long long r){
+        
+        // int res = 0;
+        
+        if(r == 0)
+            return 1;
+        
+        else
+            return (n-r+1) * NCR(n, r-1) / r;
+        
+        // return res;
+        
+    }
     vector<vector<int>> generate(int n) {
         
         vector<vector<int>> ans(n);
         
-        
-        for(int i = 0; i < n; i++){       
- 
+        for(int i = 0; i < n; i++){
+            
             ans[i].resize(i+1);
             
             for(int j = 0; j <= i; j++){
                 
-                if(j == 0 || j == i)
-                    ans[i][j] = 1;
+                int val = NCR(i, j);
                 
-                else{
-                    ans[i][j] = ans[i-1][j] + ans[i-1][j-1];
-                }
-                
+                ans[i][j] = val;
                 
             }
-  
+            
         }
+        
+        long long ncr = NCR(0, 0);
+        cout<<ncr<<" ";
+        
         
         return ans;
         
