9cf3a2d9253f8baf9cb70f5c97bac99dd6b910df
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2023-06-03 22:28:17 +0530

    Time: 526 ms (17.54%), Space: 168.1 MB (5.02%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-//     int ans= 0;
-//     void dfs(int node, unordered_map<int, list<int>>& adj, vector<int>& vis, vector<int> & time){
+    int ans= 0;
+    void dfs(int node, unordered_map<int, list<int>>& adj, vector<int> & time, int curr){
         
-//         vis[node] = 1;
-       
-//          ans += time[node];
+        ans = max(ans, curr);
         
-//         for(auto neigh : adj[node]){
-//             if(!vis[neigh]){
-                
-//                 dfs(neigh, adj, vis, time);
-//             }
-//         }
+        for(auto& neigh : adj[node]){ 
+            
+           dfs(neigh, adj, time, curr + time[node]);
+ 
+        }
         
-//     }
+    }
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
         
         unordered_map<int, list<int>> adj;
@@ -26,38 +23,38 @@ public:
             adj[manager[i]].push_back(i);
         }
         
-//         vector<int> vis(n, 0);
+        vector<int> vis(n, 0);
         
-//         dfs(headID, adj, vis, informTime);
-        
-//         return ans;
-        
-        
-        queue<pair<int,int>> q;
-        
-        q.push({headID, 0});
-        
-        int ans = 0;
-        
-        while(!q.empty()){
-            auto top = q.front();
-            
-            int node = top.first;
-            int currTime = top.second;
-            
-            q.pop();
-            
-            ans = max(ans, currTime);
-
-                for(auto& neigh : adj[node]){
- 
-                        q.push({neigh, currTime + informTime[node]});
-
-                }
-  
-        }
+        dfs(headID, adj, informTime, 0);
         
         return ans;
+        
+        
+//         queue<pair<int,int>> q;
+        
+//         q.push({headID, 0});
+        
+//         int ans = 0;
+        
+//         while(!q.empty()){
+//             auto top = q.front();
+            
+//             int node = top.first;
+//             int currTime = top.second;
+            
+//             q.pop();
+            
+//             ans = max(ans, currTime);
+
+//                 for(auto& neigh : adj[node]){
+ 
+//                         q.push({neigh, currTime + informTime[node]});
+
+//                 }
+  
+//         }
+        
+//         return ans;
         
     }
 };
