79856f6ef35197bc956db3ab479c37fb0b078ae0
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2023-06-06 20:44:47 +0530

    Time: 120 ms (67.45%), Space: 61.3 MB (51.26%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -4,27 +4,17 @@ public:
         
         int n = nums.size();
         
-        int slow = nums[0];
-        int fast = nums[0];
-        
-        do{
-             slow = nums[slow];
-            fast = nums[nums[fast]];
-        } while(slow != fast);
-        
-        // while(slow != fast){
-        //     slow = nums[slow];
-        //     fast = nums[nums[fast]];
-        // }
-        
-        fast = nums[0];
-        
-        while(slow != fast){
-            slow = nums[slow];
-            fast = nums[fast];
+        for(int i = 0; i < n; i++){
+            int ind = abs(nums[i])-1;
+            
+            //mark visited
+            nums[ind] *= -1;
+            
+            if(nums[ind] > 0)
+                return abs(nums[i]);
+            
         }
-        
-        return slow;
+        return -1;
         
     }
 };
