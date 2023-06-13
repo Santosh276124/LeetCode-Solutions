92866a0c18e588196d207e6522a2538e2abab07b
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2023-06-13 22:44:29 +0530

    Time: 19 ms (65.45%), Space: 9 MB (38.25%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -3,31 +3,39 @@ public:
     int lengthOfLongestSubstring(string s) {
         
         int n = s.length();
-        int len = 0;
+        
+        int currLen = 0;
+        int maxLen = 0;
+        
+        unordered_map<char,int> mp;
+        
         int j = 0;
-        
-        unordered_map<char, int> m;
-        
-        for(int i = 0; i < n; i++)
-        {
-            m[s[i]]++;
+        int i = 0;
+        for( ; i < n; i++){
             
-            if(m.size() == i-j+1)
-                len = max(len, i-j+1);
+            char ch = s[i];
             
-            else if(m.size() < i-j+1)
-            {
-                while(m.size() < i-j+1)
-                {
-                    m[s[j]]--;
-                    if(m[s[j]] == 0)
-                        m.erase(s[j]);
-                    
-                    j++;
-                }
+            mp[ch]++;
+            
+            currLen = i-j+1;
+            
+            while(currLen > mp.size()){
+                mp[s[j]]--;
+                if(mp[s[j]] == 0)
+                    mp.erase(s[j]);
+                
+                j++;
+                
+                currLen = i-j+1;
             }
+            
+            maxLen = max(maxLen, currLen);
+  
         }
         
-        return len;
+        // maxLen = max(maxLen, i-j);
+        
+        return maxLen;
+        
     }
 };
