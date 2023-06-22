d3b11fe6c11b372c53a2e7f347e3063b3585a4c1
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2023-06-23 01:11:51 +0530

    Time: 28 ms (71.61%), Space: 22.5 MB (9.85%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -7,17 +7,25 @@ public:
         int l = 0;
         int h = n-1;
         
-//         while(l < h){
-//             int m = l + (h-l)/2;
+        while(l <= h){
+            int m = l + (h-l)/2;
             
-//             if(nums[m] != nums[m+1] )
-//         }
+            if(m % 2 == 0){
+                if(m+1 < n && nums[m+1] == nums[m])
+                    l = m+1;
+                else
+                    h = m-1;
+            }
+            else{
+                if(m-1 >= 0 && nums[m] == nums[m-1])
+                    l = m+1;
+                else
+                    h = m-1;
+            }
+            
+        }
         
-        int xr  = 0;
-        for(auto el : nums)
-            xr ^= el;
-        
-        return xr;
+       return nums[l];
         
     }
 };
