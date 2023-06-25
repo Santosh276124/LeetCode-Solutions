546ebc7f631b21fd5e47ee62b4b3162a651a973c
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2023-06-25 18:54:43 +0530

    Time: 340 ms (38.25%), Space: 134.7 MB (48.99%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -5,27 +5,23 @@ public:
         int n = nums.size();
         vector<int> ans;
         
-        priority_queue<pair<int,int>> pq;
-        
-        int j = 0;
+        deque<int> dq;
         
         for(int i = 0; i < n; i++){
             
-            while(i-j+1 <= k){
-                pq.push({nums[i], i});
-                i++;
+            //remove out of bound el
+            if(!dq.empty() && dq.front() == i-k)
+                dq.pop_front();
+            
+            //remove lesser el from curr window
+            while(!dq.empty() && nums[dq.back()] <= nums[i])
+                dq.pop_back();
+            
+            dq.push_back(i);
+            
+            if(i >= k-1){
+                ans.push_back(nums[dq.front()]);
             }
-            
-            i--;
-            
-            ans.push_back(pq.top().first);
-            
-            while(!pq.empty() && pq.top().second <= j){
-                pq.pop();
-            }
-            
-          
-            j++;
             
         }
         
