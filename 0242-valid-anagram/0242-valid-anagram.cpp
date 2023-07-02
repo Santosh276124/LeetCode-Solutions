85fe95e8b9d0d7fe37081dcbf282f74811cc9837
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2023-07-02 19:25:35 +0530

    Time: 16 ms (41.87%), Space: 7.4 MB (13.11%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         
-        sort(s.begin(), s.end());
+        if(s == t) return true;
         
+        sort(s.begin(), s.end());
         sort(t.begin(), t.end());
         
-        if(s == t) return true;
-        else return false;
+        return s == t;
+        
     }
 };
