98e5eb46a05319e5a34590874f6e60f9b08514f3
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2023-07-07 15:11:40 +0530

    Time: 7 ms (51.11%), Space: 15.6 MB (5.66%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -2,7 +2,7 @@
 
 class Solution {
 public:
-    void solve(TreeNode* root, int r, int c, map<int, map<int,vector<int> > >& mp){
+    void solve(TreeNode* root, int r, int c, map<int, map<int,multiset<int> > >& mp){
         
         if(root == NULL) return;
         
@@ -12,36 +12,24 @@ public:
         
         solve(root->right, r+1, c+1, mp);
         
-        mp[c][r].push_back(root->val);
+        mp[c][r].insert(root->val);
         
     }
     vector<vector<int>> verticalTraversal(TreeNode* root) {
         
-        map<int, map<int,vector<int> > > mp;
+        map<int, map<int,multiset<int> > > mp;
         
         solve(root, 0, 0, mp);
         
         vector<vector<int>> ans;
         
         for(auto i : mp){
-          // cout<<i.first<<":";
             vector<int> temp;
             for(auto j : i.second){  
-                // cout<<j.first<<"->";
-                vector<int> temp1;
                 for(auto k : j.second){
-                    // cout<<k<<" ";
-                    temp1.push_back(k);
+                    temp.push_back(k);
                 } 
-                
-                sort(temp1.begin(), temp1.end());
-                for(auto el : temp1)
-                    temp.push_back(el);
-                // cout<<endl;
-                // sort(temp.begin(), temp.end());
             }
-            // cout<<endl;
-            
             ans.push_back(temp);
             
         }
