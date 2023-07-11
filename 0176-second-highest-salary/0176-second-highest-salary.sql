7327283065a47c75ff3f988fbe312b12f7879381
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2023-07-12 00:40:30 +0530

    Time: 642 ms (10.84%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,5 +1,21 @@
 
-SELECT max(salary) AS SecondHighestSalary FROM Employee
-WHERE salary NOT IN (SELECT max(salary) FROM Employee)
+# SELECT max(salary) AS SecondHighestSalary FROM Employee
+# WHERE salary NOT IN (SELECT max(salary) FROM Employee)
+
+
+with cte as (
+            select salary,
+                    dense_rank() over(order by salary desc) as rn
+            from employee
+)
+
+-- select * from cte
+
+select max(salary) as SecondHighestSalary from cte 
+where rn = 2
+    
+
+    
+
 
 
