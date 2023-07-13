c7accbefcb23135e17221205fe13456e32be26a0
SANTOSH CHAUHAN <107339803+Santosh276124@users.noreply.github.com>
2023-07-13 12:03:28 +0530

    Time: 30 ms (28.76%), Space: 14.3 MB (28.92%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,54 +1,52 @@
 class Solution {
-   
-    
 public:
-    vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
+    vector<int> findOrder(int numCourses, vector<vector<int>>& pre) {
         
-        unordered_map<int,list<int> > adj;
-        for(int i = 0; i < prerequisites.size(); i++)
-        {
-            int u = prerequisites[i][0];
-            int v = prerequisites[i][1];
+        int n = pre.size();
+        
+        unordered_map<int,list<int>> adj;
+        vector<int> indegree(numCourses);
+        
+        for(int i=0; i < n; i++){
+            
+           int u = pre[i][0];
+            int v= pre[i][1];
             
             adj[v].push_back(u);
+            
         }
         
-         vector<int> in(n, 0);
-	    
-	    for(int i = 0; i < n; i++)
-	    {
-	        for(auto neigh : adj[i])
-	        {
-	            in[neigh]++;
-	        }
-	    }
-	    
-	    queue<int> q;
-	    for(int i = 0;i < n; i++)
-	    {
-	        if(in[i] == 0)
-	            q.push(i);
-	    }
-	    
-	    vector<int> topo;
-	    while(!q.empty())
-	    {
-	        int node = q.front();
-	        q.pop();
-	        topo.push_back(node);
-	        
-	        for(auto neigh : adj[node])
-	        {
-	            in[neigh]--;
-	            if(in[neigh] == 0)
-	                q.push(neigh);
-	        }
-	    }
-	    
-	    if(topo.size() == n) return topo;
-	    return {};
-	    
+        for(int i = 0; i < numCourses; i++){
+            for(auto neigh : adj[i])
+            {
+                indegree[neigh]++;
+            }  
+        }
         
+        queue<int> q;
         
+        for(int i= 0; i < numCourses; i++){
+            if(indegree[i] == 0)
+                q.push(i);
+        }
+        
+        vector<int> ans;
+        int cnt = 0;
+        while(!q.empty()){
+            
+            auto node = q.front();
+            q.pop();
+            
+            ans.push_back(node);
+            // cnt++;
+            for(auto neigh : adj[node]){
+                indegree[neigh]--;
+                if(indegree[neigh] == 0)
+                    q.push(neigh);
+            }
+        }
+        
+        if(ans.size() == numCourses) return ans;
+        return {};
     }
 };
